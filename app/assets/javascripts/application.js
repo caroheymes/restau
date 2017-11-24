49becd27a089b792bae373c842903a01317fa518
Caroline Heymes <caroline.heymes@gmail.com>
2017-11-24 00:54:08 +0000

    commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,4 +3,35 @@
 //= require bootstrap-sprockets
 //= require uikit-icons.min.js
 //= require uikit.min.js
+//= require cloudinary
 //= require_tree .
+$(document).ready(function(){
+    $('.video').hide();
+    
+    $('#family').click(function(){
+        $('#family-video').fadeToggle();
+        $.cloudinary.videoTag("je-te-promets_xsxe0h", {autoplay: true});
+        $('#friends-video').hide();
+        $('#wedding-video').hide();
+        $('#war-video').hide();
+        
+    });
+    $('#friends').click(function(){
+        $('#friends-video').fadeToggle();
+        $('#wedding-video').hide();
+        $('#family-video').hide();
+        $('#war-video').hide();
+    });
+    $('#wedding').click(function(){
+        $('#wedding-video').fadeToggle();
+        $('#war-video').hide();
+        $('#family-video').hide();
+        $('#friends-video').hide();
+    });
+     $('#war').click(function(){
+        $('#war-video').fadeToggle();
+        $('#wedding-video').hide();
+        $('#family-video').hide();
+        $('#friends-video').hide();
+    });
+});
