8351430cb793e0fed8b772d0dcb164b8eee43fa7
Caroline Heymes <caroline.heymes@gmail.com>
2017-11-27 18:11:45 +0000

    contrôles vidéo OK

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,32 +8,34 @@
 $(document).ready(function(){
     $('.videos').hide();
     
-    $('#family').click(function(){
-        $('#family-video').fadeToggle();
-        //$.cl_videoTag("je-te-promets_xsxe0h", {autoplay: true}).transformation().toHtml());
+    $('#dtc').click(function(){
+        $('#map h3').show();
+        $('#map').show();
+        $('#mapdtc').show();
         $('#friends-video').hide();
         $('#wedding-video').hide();
         $('#war-video').hide();
         
     });
     $('#friends').click(function(){
-        $('#friends-video').fadeToggle();
+        $('#friends-video').show();
+        $('#mapdtc').hide();
+        $('#map').hide();
         $('#wedding-video').hide();
         $('#family-video').hide();
         $('#war-video').hide();
     });
     $('#wedding').click(function(){
-        $('#wedding-video').fadeToggle();
+        $('#wedding-video').show();
+        $('#map').hide();
+        $('#mapdtc').hide();
         $('#war-video').hide();
         $('#family-video').hide();
         $('#friends-video').hide();
     });
      $('#war').click(function(){
-        //$('#Blood_Toil_Tears_and_Sweat_uzbbk1').get(0).play().;
-        //$('#war-video').click(function() {
-        //this.paused ? this.play() : this.pause();
-        //$('#war-video').get(0).play().;
-    //});
+        $('#mapdtc').hide();
+        $('#map').hide();
         $('#war-video').fadeToggle();
         $('#wedding-video').hide();
         $('#family-video').hide();
@@ -42,3 +44,21 @@ $(document).ready(function(){
     
     
 });
+
+
+var myLatlng = new google.maps.LatLng( 40.3520724,-7.6122754,15);
+
+  var myOptions = {
+    zoom: 10,
+    center: myLatlng,
+    scrollwheel: false,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
+  };
+
+  var map = new google.maps.Map(document.getElementById('map'), myOptions);
+  var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title:"#Trou du cul du monde en vrai de vrai"
+  });
